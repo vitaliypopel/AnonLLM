1770a2df73dd6ff5671a994de62a9b4ec6bf0e3c
Vitalik <112088383+vitaliypopel@users.noreply.github.com>
2024-05-16 15:31:36 +0300

    Added utilities

--- project/app/views.py
+++ project/app/views.py
@@ -6,37 +6,10 @@ from django.views.decorators.http import require_http_methods
 from .models import Message
 
 from openai import InternalServerError
-
-from http.cookies import SimpleCookie
-from hashlib import md5
 import json
 
+from .utils import get_session_id, get_history
 from .llm import llm
-
-
-def get_session_id(request: HttpRequest) -> str | None:
-    # This function is creating unique hashed string for current chat using md5 for hash csrf token
-
-    cookie_string = request.headers.get('Cookie')
-    cookie = SimpleCookie()
-    cookie.load(cookie_string)
-
-    try:
-        csrf = cookie['csrftoken'].value
-        return md5(csrf.encode()).hexdigest()
-    except KeyError:
-        return None
-
-
-def get_history(messages: list[Message]) -> list[dict]:
-    # History is a history of chat which saved in database for get more detail of current chat for LLM
-
-    history = []
-    for message in messages:
-        history.append({'role': 'user', 'content': message.question})
-        history.append({'role': 'assistant', 'content': message.answer})
-
-    return history
 
 
 @require_http_methods(['GET'])
