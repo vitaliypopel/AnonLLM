d158c51bf57f8b78e107a50f27e42636b0fd8c7e
vitaliypopel <vitaliypopel14@gmail.com>
2024-07-30 17:51:20 +0300

    ready to deploy mb

--- project/project/settings.py
+++ project/project/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-#jey$7un$px#45#rgg=tk7g+)gf+45gono^lcz5qtbv6h+-3v4
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # If you want you can turn it on
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '*'
