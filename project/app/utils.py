fe759abdf6bc85499679365b054b7202cc9b84f3
vitaliypopel <vitaliypopel14@gmail.com>
2024-07-30 15:19:15 +0300

    utils bug upd

--- project/app/utils.py
+++ project/app/utils.py
@@ -3,22 +3,15 @@ from django.http import HttpRequest
 from .models import Message
 
 from hashlib import md5
-from http.cookies import SimpleCookie
 
 
 def get_session_id(request: HttpRequest) -> str | None:
     # This function is creating unique hashed string for current chat using md5 for hash csrf token
 
-    cookie_string = request.headers.get('Cookie')
-    cookie = SimpleCookie()
-    cookie.load(cookie_string)
+    csrf_token = request.COOKIES.get('csrftoken')
 
-    try:
-        csrf = cookie['csrftoken'].value
-        return md5(csrf.encode()).hexdigest()
-    except KeyError:
-        return None
-
+    if csrf_token:
+        return md5(csrf_token.encode()).hexdigest()
 
 def get_history(messages: list[Message]) -> list[dict]:
     # History is a history of chat which saved in database for get more detail of current chat for LLM
