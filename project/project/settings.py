92846f850b211aba22bb2ee50a2486b417b6d474
vitaliypopel <vitaliypopel14@gmail.com>
2024-07-30 15:25:00 +0300

    settings upd

--- project/project/settings.py
+++ project/project/settings.py
@@ -121,7 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'app/static/app'
+STATICFILES_DIRS = [
+    BASE_DIR / 'app/static/app'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
